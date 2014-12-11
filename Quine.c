4e90864dd48e4becebe6ff1248a54911c8f15997
kswaswa <kswa1@umbc.edu>
2014-12-11 14:11:40 -0500

    Update Quine.c

--- Quine.c
+++ Quine.c
@@ -1,15 +1,17 @@
-#define SIZE 22
-int Q[SIZE];
+#define SIZE 23
+string Q[SIZE];
+int i, j, k;
 int pos = -1;
 
-insQ(int e) {
+insQ(string e) {
   ++pos;
   Q[pos] = e;
 }
 
 main() {
   insQ("#define SIZE 50");
-  insQ("int Q[SIZE];");
+  insQ("string Q[SIZE];");
+  insQ("int i, j, k;");
   insQ("int pos = -1;");
   insQ("");
   insQ("insQ(int e) {");
@@ -21,22 +23,22 @@ main() {
   insQ("  insQ(");
   insQ(");");
   insQ("");
-  insQ("  for (int i = 0; i < 10; i++)");
+  insQ("  for (i = 0; i < 11; i++)");
   insQ("    printf(Q[i]);");
   insQ("");
-  insQ("  for (int i = 0; i < SIZE; i++)");
-  insQ("    printf(Q[10] + \" + Q[i] + \" + Q[11]);");
+  insQ("  for (j = 0; i < SIZE; i++)");
+  insQ("    printf(Q[11] + \" + Q[i] + \" + Q[12]);");
   insQ("");
-  insQ("  for (int i = 12; i < SIZE; i++)");
+  insQ("  for (k = 13; i < SIZE; i++)");
   insQ("    printf(Q[i]);");
   insQ("}");
   
-  for (int i = 0; i < 10; i++)
+  for (i = 0; i < 11; i++)
     printf(Q[i]);
 
-  for (int i = 0; i < SIZE; i++)
-    printf(Q[10] + \" + Q[i] + \" + Q[11]);
+  for (j = 0; i < SIZE; i++)
+    printf(Q[11] + \" + Q[i] + \" + Q[12]);
 
-  for (int i = 12; i < SIZE; i++)
+  for (k = 13; i < SIZE; i++)
     printf(Q[i]);
 }
