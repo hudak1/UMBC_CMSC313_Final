dcce7b19715a37168d33760bf96989662001576f
kswaswa <kswa1@umbc.edu>
2014-12-11 15:58:29 -0500

    Update Quine.c

--- Quine.c
+++ Quine.c
@@ -3,32 +3,42 @@
 
 main() {
   int i, j, k;
-  const char *quote = (const char *)22;
-  const char *Q[18] = {"#include <string.h>",
+  const char *quote = (const char *)34;
+  const char *Q[23] = {"#include <string.h>",
 		       "#include <stdio.h>",
 		       "",
 		       "main() {", 
 		       "  int i, j, k;",
-		       "  const char *quote = (const char *)22",
+		       "  const char *quote = (const char *)34",
 		       "  ",
 		       ",",
 		       "", 
 		       "  for (i = 0; i < 6; i++)", 
-		       "    printf(\"%s\", Q[i]);", 
+		       "    puts(Q[i]);", 
 		       "", 
-		       "  for (j = 0; i < 18; i++)", 
-		       "    printf(\"%s%s%s%s%s\", Q[6], quote, Q[i], quote, Q[7]);", 
+		       "  for (j = 0; i < 23; i++) {",
+		       "    puts(Q[6]);",
+		       "    puts(quote);",
+		       "    puts(Q[i]);",
+		       "    puts(quote);",
+		       "    puts(Q[7]);",
+		       "  }",
 		       "", 
-		       "  for (k = 8; k < 18; k++)", 
-		       "    printf(\"%s\", Q[i]);", 
+		       "  for (k = 8; k < 23; k++)", 
+		       "    puts(Q[i]);", 
 		       "}"};
   
   for (i = 0; i < 6; i++)
-    printf("%s", Q[i]);
+    puts(Q[i]);
 
-  for (j = 0; j < 18; j++)
-    printf("%s%s%s%s%s", Q[6], quote, Q[i], quote, Q[7]);
+  for (j = 0; j < 23; j++) {
+    puts(Q[6]);
+    puts(quote);
+    puts(Q[i]);
+    puts(quote);
+    puts(Q[7]);
+  }
 
-  for (k = 8; k < 18; k++)
-    printf("%s", Q[i]);
+  for (k = 8; k < 23; k++)
+    puts(Q[i]);
 }
